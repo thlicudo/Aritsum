954440e162ed38246bf5273e813903c175d566fb
Tom <thdlicudo@gmail.com>
2025-01-24 13:21:56 +0800

    added github link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
         <button
           type="button"
-          onClick={() => window.open("")}
+          onClick={() => window.open("https://github.com/thlicudo/Aritsum")}
           className="black_btn"
         >
           Github
